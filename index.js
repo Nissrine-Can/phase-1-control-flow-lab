3556a519771eca5bd190c6ad536482729cc69e09
Nissrine Canina <niss.canina@gmail.com>
2021-08-25 10:31:58 -0400

    commit tree 12784259324518188ac756fa4d0a7983a88ca920

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(ride){
+  
+   if (ride <= 400) {
+    return "This one is on me!"
+   }else if (ride > 400 && ride <= 2000) {
+      return "Charge is twenty dollars."
+  } else if (ride > 2000 &&  ride <= 2500) {
+    return "I will gladly take your thirty bucks."
+  } else {
+    return "No can do."
+  } 
+} 
+scuberGreetingForFeet(199);
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function ternaryCheckCity(destination){
+  
+   return destination === "NYC" ? "Ok, sounds good." : "No go."
+
 }
+ternaryCheckCity('Pittsburgh');
+
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case 'generous' : 
+            return 'Thank you so much.'
+    case 'not as generous' :
+            return 'Thank you.'
+    default : 
+            return 'Bye.'
+  }
+}
+switchOnCharmFromTip('thanks for everything');
